ba0bace4af19172da276993c1211ed5200d4559d
webferrol <xurxo@webferrol.com>
2020-12-14 21:15:45 +0100

    Creación del método getJSON en trait.model.php

--- libs/trait.model.php
+++ libs/trait.model.php
@@ -1,20 +1,44 @@
 <?php
 trait LOWF_Model{
-    function WFgetOptions():?object{
-        $json = "{\"login_headerurl\":\"https://www.webferrol.com\",
-                  \"login_headertext\":\"Graciñas por contactar con WefFerrol\",
-                  \"login_errors\":0,
-                  \"css_webferrol\":1,
-                  \"image_url\":\"\"
-                }";        
-        $options = get_option('LOWF_options',false);
-        return $options?json_decode($options):json_decode($json);
+    /**
+     * Obtener los valors de prefix_options. En caso de no existir obtenemos los valores por defecto
+     *
+     * @return object|null
+     */
+    function WFgetOptions():?object{      
+        $options = get_option('LOWF_options',false);        
+        return $options?json_decode($options):LOWF_Model::getJSON();
+    }
+    /**
+     * Obtener el objeto de un json por defecto por si no existe los valores en la tabla prefix_options
+     *
+     * @return object
+     */
+    public static function getJSON():object{
+        return json_decode("{\"login_headerurl\":\"https://www.webferrol.com\",
+            \"login_headertext\":\"Graciñas por contactar con WefFerrol\",
+            \"login_errors\":0,
+            \"css_webferrol\":1,
+            \"image_url\":\"\"
+        }"); 
     }
 
+    /**
+     * Obtención de la ruta de un directorio a partir de la ruta raiz del plugin
+     *
+     * @param string $file Direcotorio que deseamos obtener
+     * @return string
+     */
     public static function obtainPath(string $file='libs'):string{
         return wp_normalize_path(plugin_dir_path(LOWF_ROOTFILE)."$file/");
     }
 
+    /**
+     * Obtención de la URL a partir de la ruta raiz del plugin
+     *
+     * @param string $url
+     * @return string
+     */
     public static function obtainURL(string $url='public/css'):string{
         return plugin_dir_url(LOWF_ROOTFILE)."$url/";
     }
